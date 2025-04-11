89ef2a886e1b533c37f9c840d43cb1936e63ec77
Benjamin Ruppik <benjamin.ruppik@hhu.de>
2025-04-11 22:28:00 +0200

    Small edits to syncing scripts to make them compatible with current local HPC tools; deactivating the PCA step in the t-SNE projection and skip saving of the .html plots so save disk space.

--- grokking/scripts/train_grokk.py
+++ grokking/scripts/train_grokk.py
@@ -41,7 +41,7 @@ from torch.utils.data import DataLoader, IterableDataset
 from tqdm.auto import tqdm
 
 import wandb
-from grokking.config_classes.local_estimates.plot_config import LocalEstminatesPlotConfig
+from grokking.config_classes.local_estimates.plot_config import LocalEstminatesPlotConfig, PlotSavingConfig
 from grokking.grokk_replica.datasets import AbstractDataset
 from grokking.grokk_replica.grokk_model import GrokkModel
 from grokking.grokk_replica.load_objs import load_item
@@ -589,7 +589,14 @@ def train(
                                 msg="Creating projection plot ...",
                             )
 
-                        local_estimates_plot_config = LocalEstminatesPlotConfig()
+                        local_estimates_plot_config = LocalEstminatesPlotConfig(
+                            pca_n_components=None,  # Skip the PCA step
+                            saving=PlotSavingConfig(
+                                save_html=False,  # Since the .html is quite large, we skip saving it for now
+                                save_pdf=True,
+                                save_csv=True,
+                            ),
+                        )
 
                         saved_plots_local_estimates_root_dir = pathlib.Path(
                             output_dir,
