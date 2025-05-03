4392056d1d00f57f5c794db40cd2f6fb334b2837
Benjamin Ruppik <benjamin.ruppik@hhu.de>
2025-05-03 10:19:27 +0200

    Adding the new wand project with the different data portions to the syncing script.

--- grokking/scripts/sync_wandb_runs.sh
+++ grokking/scripts/sync_wandb_runs.sh
@@ -36,6 +36,7 @@ WANDB_OUTPUT_DIR_PATH="${!THIS_REPOSITORY_BASE_PATH_ENV_VAR_NAME}/wandb"
 
 WANDB_PROJECT_NAMES=(
   "grokking_replica"
+  "grokking_replica_HHU_Hilbert_HPC_runs_different_dataset_portions_long"
   "grokking_replica_HHU_Hilbert_HPC_runs_different_operations_and_p_values_long_with_regularization"
   "grokking_replica_HHU_Hilbert_HPC_runs_different_operations_and_p_values_long"
   "grokking_replica_HHU_Hilbert_HPC_runs_very_long"
