40423eb6ac12927d9a4f561af072b1636f2990b5
Benjamin Ruppik <benjamin.ruppik@hhu.de>
2025-09-19 00:34:46 +0200

    Remove superfluous header with author information and copyright notice from multiple files, this information is contained once in the LICENSE.

--- grokking/logging/create_and_configure_global_logger.py
+++ grokking/logging/create_and_configure_global_logger.py
@@ -1,19 +1,3 @@
-# Copyright 2025
-# [ANONYMIZED_INSTITUTION],
-# [ANONYMIZED_FACULTY],
-# [ANONYMIZED_DEPARTMENT]
-#
-# Authors:
-# AUTHOR_1 (2025) (author1@example.com)
-#
-# Code generation tools and workflows:
-# First versions of this code were potentially generated
-# with the help of AI writing assistants including
-# GitHub Copilot, ChatGPT, Microsoft Copilot, Google Gemini.
-# Afterwards, the generated segments were manually reviewed and edited.
-#
-
-
 """Create and configure a global logger."""
 
 import logging
